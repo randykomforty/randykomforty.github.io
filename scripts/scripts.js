e82a1a3108312c130345681b70f77926ffe2da34
Randy Komforty <randykomforty@gmail.com>
2025-01-09 17:56:24 -0800

    update home, crum, vycichl, westendorf

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,22 @@
-var elements = document.querySelectorAll("h2, h3");
-elements.forEach(element => {
-	element.addEventListener("click", function (e) {
-		console.log("This is the #" + element.parentElement.id + " element.");
-		element.parentNode.classList.toggle("collapse");
+var elementsH2 = document.querySelectorAll("h2");
+var elementsH3 = document.querySelectorAll("h3");
+var children = document.querySelectorAll(":has(> :is(h2, h3)) > :not(h2):not(h3)");
+var entries = document.querySelectorAll(".entry");
+
+elementsH2.forEach(elementH2 => {
+	elementH2.addEventListener("click", function (e) {
+		console.log("This is the #" + elementH2.parentElement.id + " element.");
+		elementH2.parentNode.classList.toggle("collapse");
+	});
+});
+elementsH3.forEach(elementH3 => {
+	elementH3.addEventListener("click", function (e) {
+		console.log("This is the #" + elementH3.parentElement.id + " element.");
+		elementH3.parentNode.classList.toggle("collapse");
+	});
+});
+entries.forEach(entry => {
+	entry.addEventListener("click", function (e) {
+		entry.parentNode.classList.toggle("collapse");
 	});
 });
