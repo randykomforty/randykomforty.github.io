c91bbbfc9336208baa747e015c9c726d88ae92fd
Randy Komforty <randykomforty@gmail.com>
2025-01-30 17:59:09 -0800

    update dictionary editor

--- coptic/dictionary_editor/scripts/scripts.js
+++ coptic/dictionary_editor/scripts/scripts.js
@@ -34,18 +34,18 @@ function applyRegexes(x) {
 	//const indent = [/\t/, "<span class=\"indent-space\"> </span>"];
 	const bold = [/\*\[(.*?)\]/, "<b>$1<\/b>"];
 	const italic = [/\_\[(.*?)\]/, "<i>$1<\/i>"];
-	const superscript = [/\^(\d)/, "<sup>$1<\/sup>"];
+	const superscript = [/\^(\d+)/, "<sup>$1<\/sup>"];
 	const qualitative = [/\^\+/, "<sup>†<\/sup>"];
-	const dialect = [/\_d\[(.)\]/, "<i class=\"dialect\">$1<\/i>"];
-	const subdialect = [/\_d\[(.)([a-zA-Z])\]/, "<i class=\"dialect\">$1<sup>$2<\/sup><\/i>"];
-	const subdialectLyco = [/\_d\[(.)([0-9])\]/, "<i class=\"dialect lycopolitan\">$1<sup>$2<\/sup><\/i>"];
+	const dialect = [/\_d\[(\w)\]/, "<i class=\"dialect\">$1<\/i>"];
+	const subdialect = [/\_d\[(\w)([a-zA-Z])\]/, "<i class=\"dialect\">$1<sup>$2<\/sup><\/i>"];
+	const subdialectLyco = [/\_d\[(\w)([0-9])\]/, "<i class=\"dialect lycopolitan\">$1<sup>$2<\/sup><\/i>"];
 	const headword = [/\~co1\[(.*?)\]/, "<span class=\"headword coptic\">$1<\/span>"];
 	const coptic = [/\~co\[(.*?)\]/, "<span class=\"coptic\">$1<\/span>"];
 	const greek = [/\~gr\[(.*?)\]/, "<span class=\"greek\">$1<\/span>"];
 	const arabic = [/\~ab\[(.*?)\]/, "<span class=\"arabic\">$1<\/span>"];
 	const hebrew = [/\~he\[(.*?)\]/, "<span class=\"hebrew\">$1<\/span>"];
 	const aramaic = [/\~am\[(.*?)\]/, "<span class=\"aramaic\">$1<\/span>"];
-	const arrayStyling = [bold, italic, superscript, qualitative, dialect, subdialect, subdialectLyco, headword, coptic, greek, arabic, hebrew, aramaic];
+	let arrayStyling = [bold, italic, superscript, qualitative, dialect, subdialect, subdialectLyco, headword, coptic, greek, arabic, hebrew, aramaic];
 	const liOpen = [/{-(?!-)/, "<li>"];
 	const liOpenSubsection = [/{s-/, "<li class=\"subsection\">"];
 	const liOpenGroup = [/{g-/, "<li class=\"group\">"];
@@ -63,5 +63,25 @@ function applyRegexes(x) {
 		let regex = new RegExp(arrayStyling[j][0], "msg");
 		processedText = processedText.replace(regex, arrayStyling[j][1]);
 	}
+	let moreProcessedText = processedText;
+	const boldEdit = [/<b>(.*?)<\/b>/, "<b class=\"discrete\">$1<\/b>"];
+	const italicEdit = [/<i>(.*?)<\/>/, "<i class=\"discrete\">$1<\/i>"];
+	const superscriptEdit = [/(?<!<i class=\"dialect\">.*?)<sup>(\d+)<\/sup>/, "<sup class=\"discrete\">$1<\/sup>"];
+	const qualitativeEdit = [/<sup>†<\/sup>/, "<sup class=\"discrete\">†<\/sup>"];
+	const dialectEdit = [/<i class=\"dialect\">(\w)<\/i>/, "<i class=\"dialect discrete\">$1<\/i>"];
+	const subdialectEdit = [/<i class=\"dialect\">(\w)<sup>([a-zA-Z])<\/sup><\/i>/, "<i class=\"dialect discrete\">$1<sup>$2<\/sup><\/i>"];
+	const subdialectLycoEdit = [/<i class=\"dialect lycopolitan\">(\w)<sup>([0-9])<\/sup><\/i>/, "<i class=\"dialect lycopolitan discrete\">$1<sup>$2<\/sup><\/i>"];
+	const headwordEdit = [/<span class=\"headword coptic\">(.*?)<\/span>/, "<span class=\"headword coptic discrete\">$1<\/span>"];
+	const copticEdit = [/<span class=\"coptic\">(.*?)<\/span>/, "<span class=\"coptic discrete\">$1<\/span>"];
+	const greekEdit = [/<span class=\"greek\">(.*?)<\/span>/, "<span class=\"greek discrete\">$1<\/span>"];
+	const arabicEdit = [/<span class=\"arabic\">(.*?)<\/span>/, "<span class=\"arabic discrete\">$1<\/span>"];
+	const hebrewEdit = [/<span class=\"hebrew\">(.*?)<\/span>/, "<span class=\"hebrew discrete\">$1<\/span>"];
+	const aramaicEdit = [/<span class=\"aramaic\">(.*?)<\/span>/, "<span class=\"aramaic discrete\">$1<\/span>"];
+	arrayStyling = [boldEdit, italicEdit, superscriptEdit, qualitativeEdit, subdialectLycoEdit, dialectEdit, subdialectEdit, headwordEdit, copticEdit, greekEdit, arabicEdit, hebrewEdit, aramaicEdit];
+	for (let j = 0; j < arrayStyling.length; j++) {
+		let regex = new RegExp(arrayStyling[j][0], "msg");
+		moreProcessedText = moreProcessedText.replace(regex, arrayStyling[j][1]);
+	}
+	editorText.innerHTML = moreProcessedText;
 	previewRendered.innerHTML = processedText;
 }
