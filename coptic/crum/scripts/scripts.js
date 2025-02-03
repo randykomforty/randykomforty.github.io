97941ba7e7ba771f3d69d2b910151fd85067b5fa
Randy Komforty <randykomforty@gmail.com>
2025-02-03 02:30:45 -0800

    update crum

--- coptic/crum/scripts/scripts.js
+++ coptic/crum/scripts/scripts.js
@@ -1,3 +1,5 @@
+const menu = document.querySelector("#menu");
+const tabMenu = document.querySelector("#menu ul");
 const tabs = document.querySelectorAll("#menu ul li");
 const tabContents = [
 	document.querySelector("#preface"),
@@ -7,13 +9,25 @@ const tabContents = [
 ];
 tabs.forEach((x, i) => {
 	x.addEventListener("click", y => {
-		console.log("Element \"" + y.target.id + "\" has been clicked.");
 		for (let i = 0; i < tabContents.length; i++) {
 			tabContents[i].classList.remove("show");
 			tabs[i].classList.remove("active");
 		}
 		tabContents[i].classList.toggle("show");
 		tabs[i].classList.toggle("active");
+		menu.scrollTo({
+			left: 0,
+			behavior: "smooth"
+		});
 	});
 });
-
+const menuObserver = new ResizeObserver((entries) => {
+	const isTall = entries[0].contentRect.height >= 64;
+	//let isSecondRow = "";
+	//for (let i = 0; i < tabs.length; i++) {
+		//isSecondRow = tabs[i].offsetTop = 48;
+		//tabs[i].style.height = isSecondRow ? "4em" : "2em";
+	//}
+	//entries[0].target.style.background = isTall ? "linear-gradient(to top, #232448 50%, transparent 50%)" : "transparent";
+});
+menuObserver.observe(tabMenu);
