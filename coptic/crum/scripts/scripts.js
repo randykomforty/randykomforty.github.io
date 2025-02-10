6e2d2749787dcfcfd5a4cfed2cea51e7d5122de8
Randy Komforty <randykomforty@gmail.com>
2025-02-10 01:19:39 -0800

    update crum

--- coptic/crum/scripts/scripts.js
+++ coptic/crum/scripts/scripts.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector("#menu");
 const tabMenu = document.querySelector("#menu ul");
-const tabs = document.querySelectorAll("#menu ul li");
+const activeTab = document.querySelector("#menu > ul > li:nth-child(1) a");
+const inactiveTabs = document.querySelectorAll("#menu ul li ul li a");
+const allTabs = document.querySelectorAll("#menu a");
 const tabContents = [
 	document.querySelector("#preface"),
 	document.querySelector("#list_of_abbreviations"),
@@ -10,25 +12,80 @@ const tabContents = [
 	document.querySelector("#greek_index"),
 	document.querySelector("#arabic_index")
 ];
+
 const entries = document.querySelectorAll(".entry > ul > li:nth-child(1)");
 const entriesExpanded = document.querySelectorAll(".entry > ul > li:nth-child(2)");
-tabs.forEach((x, i) => {
-	x.addEventListener("click", y => {
-		for (let i = 0; i < tabContents.length; i++) {
-			tabContents[i].classList.remove("show");
-			tabs[i].classList.remove("active");
-		}
-		tabContents[i].classList.toggle("show");
-		tabs[i].classList.toggle("active");
-		menu.scrollTo({
-			left: 0,
-			behavior: "smooth"
-		});
-		window.scrollTo({
-			top: 0
-		});
+
+checkUrl();
+
+activeTab.addEventListener("click", e => {
+	e.preventDefault();
+});
+
+inactiveTabs.forEach((x, i) => {
+	x.addEventListener("click", e => {
+		e.preventDefault();
+		window.history.pushState({}, "", x.href);
+		checkUrl(e.target);
+		//console.log(e.target.parentElement);
 	});
 });
+
+function checkUrl(linkToSwap) {
+	for (let i = 0; i < inactiveTabs.length; i++) {
+		inactiveTabs[i].parentElement.classList.remove("hide");
+		tabContents[i].classList.remove("show");
+	}
+	let swap = (linkToSwap || inactiveTabs[0]);
+	let abc = {};
+	abc.incomingLink = swap.href;
+	abc.incomingTitle = swap.innerHTML;
+	if (activeTab.href != abc.incomingLink) {
+		activeTab.href = abc.incomingLink;
+		activeTab.innerHTML = abc.incomingTitle;
+		swap.parentElement.classList.add("hide");
+	}
+	else {
+		inactiveTabs[0].parentElement.classList.add("hide");
+	}
+	
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const section = urlParams.get("section");
+	console.log(section);
+	switch (section) {
+		case "preface":
+			tabContents[0].classList.add("show");
+		break;
+		case "list_of_abbreviations":
+			tabContents[1].classList.add("show");
+		break;
+		case "additions_and_corrections":
+			tabContents[2].classList.add("show");
+		break;
+		case "dictionary":
+			tabContents[3].classList.add("show");
+		break;
+		case "english_index":
+			tabContents[4].classList.add("show");
+		break;
+		case "greek_index":
+			tabContents[5].classList.add("show");
+		break;
+		case "arabic_index":
+			tabContents[6].classList.add("show");
+		break;
+		default:
+			tabContents[0].classList.add("show");
+	}
+	
+}
+
+
+
+
+
+
 let findHeight = "";
 let expandedHeight = [];
 entries.forEach((x, i) => {
