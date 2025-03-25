8a4ed882f60c02eaefbf3d8835c66826cbc4ca5d
Randy Komforty <randykomforty@gmail.com>
2025-03-25 01:34:37 -0700

    make /coptic a submodule

--- coptic/bible/scripts/revelation.js
+++
@@ -1,6 +0,0 @@
-for (var i = 0; i < document.getElementsByTagName("ruby").length; i++) {
-	document.getElementsByTagName("ruby")[i].getElementsByTagName("a")[0].addEventListener("click", function (event) {
-		event.preventDefault();
-		return false;
-	});
-}
